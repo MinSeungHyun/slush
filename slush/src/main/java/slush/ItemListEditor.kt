c02a5d942c05a33071b05497600406dc082f5ed6
SeungHyunMin <contact@seunghyun.in>
2020-06-01 19:30:29 +0900

    Edit ItemListEditor.kt

--- slush/src/main/java/slush/ItemListEditor.kt
+++ slush/src/main/java/slush/ItemListEditor.kt
@@ -3,15 +3,16 @@ package slush
 interface ItemListEditor<ITEM> {
     fun addItem(item: ITEM)
     fun addItemAt(position: Int, item: ITEM)
-    fun addItemRange(startPosition: Int, items: List<ITEM>)
+    fun addItemRange(items: List<ITEM>)
+    fun addItemRangeAt(startPosition: Int, items: List<ITEM>)
 
-    fun removeItem(item: ITEM)
+    fun removeItem(item: ITEM): Boolean
     fun removeItemAt(position: Int)
     fun removeItemRange(startPosition: Int, itemCount: Int)
 
     fun changeItemAt(position: Int, item: ITEM)
-    fun changeAll(items: List<ITEM>)
     fun changeItemRange(startPosition: Int, items: List<ITEM>)
+    fun changeAll(items: List<ITEM>)
 
     fun moveItem(fromPosition: Int, toPosition: Int)
 }
