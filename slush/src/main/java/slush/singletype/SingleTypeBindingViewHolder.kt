16d01b7170736d616995162867cd44f3e52745af
SeungHyunMin <contact@seunghyun.in>
2020-06-07 08:37:46 +0900

    Create SingleTypeBindingViewHolder

---
+++ slush/src/main/java/slush/singletype/SingleTypeBindingViewHolder.kt
@@ -0,0 +1,21 @@
+package slush.singletype
+
+import androidx.databinding.ViewDataBinding
+import slush.listeners.OnBindDataListener
+import slush.listeners.OnItemClickListener
+
+class SingleTypeBindingViewHolder<ITEM>(
+    private val binding: ViewDataBinding,
+    private val onBindDataListener: OnBindDataListener<ITEM>?,
+    private val onItemClickListener: OnItemClickListener?
+) : BaseSingleTypeViewHolder<ITEM>(binding.root) {
+
+    override fun bind(position: Int, item: ITEM) {
+        onBindDataListener?.onBind(binding, item)
+
+        onItemClickListener ?: return
+        binding.root.setOnClickListener {
+            onItemClickListener.onItemClick(it, position)
+        }
+    }
+}
