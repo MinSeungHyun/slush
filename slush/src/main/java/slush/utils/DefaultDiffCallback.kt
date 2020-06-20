be1d9242601716e9fe9e0e47fe24aedfbc66af4d
SeungHyunMin <contact@seunghyun.in>
2020-06-20 16:40:29 +0900

    Create DefaultDiffCallback.kt

---
+++ slush/src/main/java/slush/utils/DefaultDiffCallback.kt
@@ -0,0 +1,30 @@
+package slush.utils
+
+class DefaultDiffCallback<ITEM> : SlushDiffCallback<ITEM>() {
+    private var oldItems = listOf<ITEM>()
+    private var newItems = listOf<ITEM>()
+
+    override fun setOldItems(oldItems: List<ITEM>) {
+        this.oldItems = oldItems
+    }
+
+    override fun setNewItems(newItems: List<ITEM>) {
+        this.newItems = newItems
+    }
+
+    override fun getOldListSize(): Int {
+        return oldItems.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newItems.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldItems[oldItemPosition] === newItems[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldItems[oldItemPosition] == newItems[newItemPosition]
+    }
+}
