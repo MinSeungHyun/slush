857afd1a52db8759ed05dd185e28dee52ad3a83a
SeungHyunMin <contact@seunghyun.in>
2020-07-31 16:58:37 +0900

    Create ItemListEditorUnavailableException

--- slush/src/main/java/slush/AdapterAppliedResult.kt
+++ slush/src/main/java/slush/AdapterAppliedResult.kt
@@ -2,10 +2,11 @@ package slush
 
 import slush.singletype.SingleTypeAdapter
 import slush.singletype.SingleTypeList
+import slush.utils.SlushException
 
 class AdapterAppliedResult<ITEM>(private val adapter: SingleTypeAdapter<ITEM>) {
     val itemListEditor: ItemListEditor<ITEM> by lazy {
         if (adapter.singleTypeList is SingleTypeList.NormalList) AdapterItemListEditor(adapter)
-        else throw Exception()
+        else throw SlushException.ItemListEditorUnavailableException()
     }
 }
--- slush/src/main/java/slush/utils/SlushException.kt
+++ slush/src/main/java/slush/utils/SlushException.kt
@@ -1,6 +1,12 @@
 package slush.utils
 
 sealed class SlushException(message: String) : Exception(message) {
-    class LayoutIdNotFoundException : SlushException("Method setItemLayout() not called")
-    class BothBindMethodsCalledException : SlushException("Only one of the methods onBind and onBindData should be called.")
+    class LayoutIdNotFoundException :
+        SlushException("Method setItemLayout() not called")
+
+    class BothBindMethodsCalledException :
+        SlushException("Only one of the methods onBind and onBindData should be called.")
+
+    class ItemListEditorUnavailableException :
+        SlushException("Cannot use ItemListEditor when not using a normal list")
 }
